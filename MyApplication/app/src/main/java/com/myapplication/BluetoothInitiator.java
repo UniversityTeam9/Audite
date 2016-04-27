8ac6b7a4de9f84eee37930d6daa277155535fea5
mayra <mayrageraldine@icloud.com>
2016-04-27 12:20:28 +0200

    the rest of the bluetooth connection
    blduetooth connection is done with the smart car

--- MyApplication/app/src/main/java/com/myapplication/BluetoothInitiator.java
+++ MyApplication/app/src/main/java/com/myapplication/BluetoothInitiator.java
@@ -66,3 +66,94 @@ public class BluetoothInitiator {
             return message;
         }
     }
+
+    private String address;
+    private UUID uuid;
+
+    private BluetoothAdapter adapter;
+    private BluetoothDevice device;
+    private BluetoothSocket socket;
+
+    /**
+     * Initialize {@code BluetoothInitiator}
+     * @param address Target device address
+     * @param uuid Application UUID
+     */
+    public BluetoothInitiator(String address, UUID uuid) {
+        this.address = address;
+        this.uuid = uuid;
+    }
+
+    /**
+     * Get current socket
+     * @return Socket
+     */
+    public BluetoothSocket getSocket() {
+        return socket;
+    }
+
+    /**
+     * Find target device
+     * @return Result from search attempt
+     */
+    public SearchResult search() {
+        adapter = BluetoothAdapter.getDefaultAdapter();
+        if (adapter == null) {
+            return SearchResult.ADAPTER_NOT_FOUND;
+        }
+
+        if (!adapter.isEnabled()) {
+            return SearchResult.ADAPTER_NOT_ENABLED;
+        }
+
+        for (BluetoothDevice device : adapter.getBondedDevices()) {
+            if (device.getAddress().equals(address)) {
+                this.device = device;
+                return SearchResult.TARGET_FOUND;
+            }
+        }
+
+        return SearchResult.TARGET_NOT_FOUND;
+    }
+
+    /**
+     * Establish connection with target device
+     * @return Result from connection attempt
+     */
+    public ConnectionResult connect() {
+        try {
+            socket = device.createRfcommSocketToServiceRecord(uuid);
+        } catch (IOException createException) {
+            createException.printStackTrace();
+            return ConnectionResult.CONNECTION_ERROR;
+        }
+
+        adapter.cancelDiscovery();
+
+        try {
+            socket.connect();
+            return ConnectionResult.CONNECTION_ESTABLISHED;
+        } catch (IOException connectException) {
+            connectException.printStackTrace();
+        }
+
+        try {
+            socket.close();
+        } catch (IOException closeException) {
+            closeException.printStackTrace();
+        }
+
+        return ConnectionResult.CONNECTION_ERROR;
+    }
+
+    /**
+     * Disconnect from target device
+     */
+    public void disconnect() {
+        try {
+            socket.close();
+        } catch (IOException closeException) {
+            closeException.printStackTrace();
+        }
+    }
+}
